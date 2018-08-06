d6691795456c38671ab9143aa52145e1536d1f26
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-08-06 21:55:06 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
-  # code goes here
+  
+  new_hash ={ }
+  array.each do |animal|
+    if new_hash[animal]
+      new_hash[animal]+=1
+    else
+      new_hash[animal] = 1
+    end  
+  end
+new_hash  
 end
  

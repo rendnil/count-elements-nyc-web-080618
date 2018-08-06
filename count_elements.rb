695f106d5336f9f81faee610ecf16c73270f893b
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-08-06 21:52:29 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,7 +1,14 @@
 def count_elements(array)
   
   new_hash ={ }
-  
+  array.each do |animal|
+    if new_hash[animal]
+      new_hash[animal]+=1
+    else
+      new_hash[animal] = 1
+      
+    end  
+  end
   
 end
  
